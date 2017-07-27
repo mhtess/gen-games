b8697347493a098a38d42d1e244a7c67b1b0285f
Andrea <ashulman18@ymail.com>
2017-07-27 16:14:59 -0700

    added page in between rounds

--- experiments/mp-game-1/js/mp-game-1.js
+++ experiments/mp-game-1/js/mp-game-1.js
@@ -138,6 +138,14 @@ function make_slides(f) {
     }
   });
 
+  slides.structure_instruct = slide({
+    name: "structure_instruct",
+    button : function() {
+      exp.go();
+    }
+  })
+
+
   // This is the learning slide in which users will uncover information about the critters
   slides.welcome_critterLand = slide({
     name : "welcome_critterLand",
@@ -411,6 +419,7 @@ function init() {
     "welcome_critterLand",
     "chatRoom",
     "test_critters",
+    "structure_instruct",
     "wait_room",
     "welcome_critterLand",
     "chatRoom",
