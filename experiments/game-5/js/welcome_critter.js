4dddf1545f645aa5f4692b7a97d30cdf0ee1b9ce
schopra8 <schopra8@stanford.edu>
2018-02-05 14:49:35 -0800

    Dataset Generation for Pilot

--- experiments/game-5/js/welcome_critter.js
+++ experiments/game-5/js/welcome_critter.js
@@ -155,7 +155,7 @@ var example = function() {
 // ----
 // MAIN
 // ----
-var numSets = 10;
+var numSets = 50;
 var easy_rule = function(critter) {
 	// Rule: If critter is orange
 	return critter["props"]["col1"] === color_dict["orange"]
