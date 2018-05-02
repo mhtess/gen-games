819bbefbc77afdea1cafd698fcf60bfaf662f27d
schopra8 <schopra8@stanford.edu>
2018-05-02 13:10:57 -0700

    More minor tweaks ...

--- experiments/sharedUtils/serverBase.js
+++ experiments/sharedUtils/serverBase.js
@@ -15,15 +15,15 @@ class ReferenceGameServer {
 
     if (this.expName === 'mp-game-3') {
       this.numGamesPerRule = {
-        "0": 0,
+        "0": 2,
         "1": 0,
         "2": 0,
         "3": 0,
         "4": 0,
         "5": 0,
         "6": 0,
-        "7": 4,
-        "8": 4,
+        "7": 3,
+        "8": 3,
         "9": 3,
       }
     }
