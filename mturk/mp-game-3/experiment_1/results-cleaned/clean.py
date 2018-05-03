4ca6cca336b71aa91f552be603973d61d267115a
schopra8 <schopra8@stanford.edu>
2018-05-02 17:31:04 -0700

    WIP: Reconstruct Mturk Files (Missing Partners) from Server Logs -- Summary Stats

---
+++ mturk/mp-game-3/experiment_1/results-cleaned/clean.py
@@ -0,0 +1,209 @@
+import pandas as pd
+from collections import defaultdict
+import os
+import pprint
+
+DIR = '../production-results'
+RAW_SERVER_LOGS = '../raw-server-data'
+RAW_SERVER_LOGS_CHAT_MESSAGES = os.path.join(RAW_SERVER_LOGS, 'chatMessage')
+RAW_SERVER_LOGS_SUMMARY_STATS = os.path.join(RAW_SERVER_LOGS, 'logScores')
+RAW_SERVER_LOGS_SUBJ_INFO = os.path.join(RAW_SERVER_LOGS, 'logSubjInfo')
+RAW_SERVER_LOGS_TEST = os.path.join(RAW_SERVER_LOGS, 'logTest')
+RAW_SERVER_LOGS_TRAIN = os.path.join(RAW_SERVER_LOGS, 'logTrain')
+
+ROLE_STUDENT = 'student'
+ROLE_EXPLORER = 'explorer'
+
+def identify_partnerless_results():
+    '''
+    Sometimes results are submitted to Mturk, when a participant disconnected from the game.
+    These leads to result files, where we only have 1/2 of the game, i.e. only the listener
+    or only the teacher. Here we print a log of the game_ids and files that have only one player.
+    '''
+    games = defaultdict(list)
+    for filename in os.listdir(DIR):
+        if filename.endswith('.json'):
+            fp = os.path.join(DIR, filename)
+            df = pd.read_json(fp)
+            game_id = df['answers']['game_id']
+            player_role = df['answers']['role']
+            rule_idx = df['answers']['rule_idx']
+            training_fn = df['answers']['training_data_fn']
+            test_fn = df['answers']['test_data_fn']
+            rule_type = df['answers']['rule_type']
+            games[game_id].append({
+                'game_id': game_id,
+                'rule_idx': rule_idx,
+                'role': player_role,
+                'training_data_fn': training_fn,
+                'test_data_fn': test_fn,
+                'rule_type': rule_type,
+            })
+        else:
+            continue
+    incomplete_games = []
+    for (game_id, players_info) in games.iteritems():
+        if len(players_info) != 2:
+            incomplete_games.append(players_info[0])
+    return incomplete_games
+
+def fill_summary_stats(game_id, role, mturk_file_struct):
+    '''
+    Given game_id, role look up summary stats in raw server logs.
+    '''
+    for filename in os.listdir(RAW_SERVER_LOGS_SUMMARY_STATS):
+        if game_id in filename:
+            fp = os.path.join(RAW_SERVER_LOGS_SUMMARY_STATS, filename)
+            df = pd.read_csv(fp, sep='	')
+            print df
+            
+            # Training Summary Stats Extraction
+            if role == ROLE_STUDENT:
+                mturk_file_struct['answers']['training_summary_stats'] = {
+                    'hits': 0,
+                    'misses': 0,
+                    'score': 0,
+                    'false_alarms': 0,
+                    'type': 'training',
+                    'correct_rejections': 0
+                }
+            else:
+                training_results_row = df.loc[(df['role'] == role) & (df['type'] == 'training')]
+                import pdb; pdb.set_trace()
+                mturk_file_struct['answers']['training_summary_stats'] = {
+                    'hits': training_results_row['hits'].values[0],
+                    'misses': training_results_row['misses'].values[0],
+                    'score': training_results_row['score'].values[0],
+                    'false_alarms': training_results_row['false_alarms'].values[0],
+                    'type': 'training',
+                    'correct_rejections': training_results_row['correct_rejections'].values[0],
+                }     
+        
+            # Testing Summary Stats Extraction
+            testing_summary_stats = df.loc[(df['role'] == role) & (df['type'] == 'testing')]
+            mturk_file_struct['answers']['testing_summary_stats'] = {
+                'hits': testing_summary_stats['hits'].values[0],
+                'misses': testing_summary_stats['misses'].values[0],
+                'score': testing_summary_stats['score'].values[0],
+                'false_alarms': testing_summary_stats['false_alarms'].values[0],
+                'type': 'testing',
+                'correct_rejections': testing_summary_stats['correct_rejections'].values[0],
+            }                          
+            break
+
+def get_train_answers(game_id, role):
+    '''
+    Given game_id, role look up training_answers in raw server logs.
+    '''
+    if role == ROLE_STUDENT:
+        return []
+    else:
+        return [] # TODO: Implement
+
+def get_test_answers(game_id, role):
+    '''
+    Given game_id, role look up training_answers in raw server logs.
+    '''
+    pass
+
+def fill_shared_info(other_player_info, missing_player_role, mturk_file_struct):
+    ''' Fill information shared between two players in provided mturk_file_struct
+    '''
+    game_id = other_player_info['game_id']
+    rule_idx = other_player_info['rule_idx']
+    training_fn = other_player_info['training_data_fn']
+    test_fn = other_player_info['test_data_fn']
+    rule_type = other_player_info['rule_type']
+    mturk_file_struct['answers']['game_id'] = game_id
+    mturk_file_struct['answers']['role'] = missing_player_role 
+    mturk_file_struct['answers']['rule_idx'] = rule_idx 
+    mturk_file_struct['answers']['training_data_fn'] = training_fn
+    mturk_file_struct['answers']['test_data_fn'] = test_fn
+    mturk_file_struct['answers']['rule_type'] = rule_type 
+
+def get_empty_mturk_struct():
+    # Create MTURK File Structure
+    mturk_file_struct = {
+        'AutoApprovalTime': '',
+        'AssignmentId': '',
+        'WorkerId': '',
+        'answers': {
+            'rule_type': '',
+            'test_data_fn': '',
+            'time_in_minutes': 'null',
+            'training_summary_stats': {
+                'hits': -1,
+                'misses': -1,
+                'score': -1,
+                'false_alarms': -1,
+                'type': 'training',
+                'correct_rejections': -1
+            },
+            'testing_trials': [],
+            'system': {
+                'screenW': -1,
+                'screenH': -1,
+                'OS': '',
+                'Browser': ''
+            },
+            'training_data_fn': '',
+            'role': '',
+            'training_trials': [],
+            'subject_information': {
+                'enjoyment': '',
+                'gender': '',
+                'age': '',
+                'problems': '',
+                'nativeEnglish': '',
+                'strategy': '',
+                'assess': '',
+                'humanPartner': '',
+                'fairprice': '',
+                'comments': '',
+                'likePartner': '',
+                'education': ''
+            },
+            'game_id': '',
+            'testing_summary_stats': {
+                'hits': -1,
+                'misses': -1,
+                'score': -1,
+                'false_alarms': -1,
+                'type': 'testing',
+                'correct_rejections': -1,
+            },
+            'rule_idx': -1,
+        },
+        'AcceptTime': '',
+        'HITId': '',
+        'Assignment': '',
+        'AssignmentStatus': '',
+        'SubmitTime': ''
+    }
+    return mturk_file_struct
+
+def construct_mturk_file(other_player_info):
+    '''
+    When we have an incomplete game, we can often recover the partner's data
+    by examining the raw data logged to the server. In order to make things easier
+    downstream, we sif through these raw logs and create a file in the fashion of one
+    that would have been pulled down from Mturk.
+    '''
+    # Extract Info from Other Player
+    mturk_file_struct = get_empty_mturk_struct()
+    game_id = other_player_info['game_id']
+    missing_player_role = ROLE_EXPLORER if other_player_info['role'] == ROLE_STUDENT else ROLE_STUDENT
+    fill_shared_info(other_player_info, missing_player_role, mturk_file_struct)
+
+    # Gather Info about Missing Player
+    fill_summary_stats(game_id, missing_player_role, mturk_file_struct)
+    pp = pprint.PrettyPrinter(indent=4)
+    pp.pprint(mturk_file_struct)
+
+
+if __name__ == '__main__':
+    pp = pprint.PrettyPrinter(indent=4)
+    incomplete_games = identify_partnerless_results()
+
+    pp.pprint(incomplete_games[0])
+    construct_mturk_file(incomplete_games[0])
